228b3a488280afe12fe027899274886c590d6c61
KonstantinAleksandrov <constantine22811@gmail.com>
2023-07-25 21:28:52 +0300

    cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import catalogRouter from './routes.js'
+import cors from 'cors'
+
 const PORT = '5005'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
+
 app.use('/catalog',catalogRouter)
 
 app.listen(PORT,()=>{
